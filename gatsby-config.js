5e0fa03ab385145a16c09aede50207aadaa7ef28
yehezkiel <yehezkiel@tokopedia.com>
2022-03-02 10:29:10 +0700

    support dark mode browser favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -79,7 +79,7 @@ module.exports = {
         background_color: "#f7f0eb",
         theme_color: "#a2466c",
         display: `minimal-ui`,
-        icon: "assets/v1.png",
+        icon: "assets/favicon.svg",
       },
     },
   ],
